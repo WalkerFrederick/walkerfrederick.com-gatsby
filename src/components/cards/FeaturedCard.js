69614518fab30ae53e0db0f2effe80ffbd4897e9
Walker Frederick <walker@urdev.xyz>
2019-04-07 17:42:15 -0400

    Enhanced the styling on FeaturedCard component

--- src/components/cards/FeaturedCard.js
+++ src/components/cards/FeaturedCard.js
@@ -20,19 +20,22 @@ class FeaturedCard extends React.Component {
         }
     }
 
+    shortifyExcerpt(excerpt) {
+        return excerpt.substring(0, 150)
+    }
+
     render() {
         return (
             <>
                 <Link to={`/${this.props.post.slug}`} className="featured-card">
                     <div className="featured-card-image">
-                    
+                        <img src={`${this.props.post.feature_image}`}></img>
                     </div>
                     <div className="featured-card-content">
                         <h1>{this.props.post.slug}</h1>
                         <span>{`Published ${prettyDate(this.props.post.published_at)}`}</span>
-                        {this.props.post.featured && <span>Featured</span>}
-                        <p>{this.props.post.excerpt}</p>
-                        <div>{readingTimeHelper(this.props.post)}</div>
+                        <p>{`${this.shortifyExcerpt(this.props.post.excerpt)}...`}</p>
+                        <div className="reading-time">{readingTimeHelper(this.props.post)}</div>
                     </div>
                 </Link>
             </>
